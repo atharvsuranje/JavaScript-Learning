e82e6222cbcb43a0fa7e7c5b533f1508f24a83ea
atharvsuranje <atharvsuranje2604@gmail.com>
2025-08-28 11:20:36 +0530

    Added dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let name="Atharv" //string
+let age=22 //int
+let isValid = true //boolean
+let state=null //standalone value not 0 or undefined
+//undefined
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
