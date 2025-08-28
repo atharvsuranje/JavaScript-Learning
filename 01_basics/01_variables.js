83d2791771ada698e377cc872efed330eeea58b0
atharvsuranje <atharvsuranje2604@gmail.com>
2025-08-28 11:11:17 +0530

    Added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const id = 12345
+let email = "atharv@gmail.com"
+var password ="1111" // isn't use much as it causes block issues
+city = "Pune" // allowed but prefer not to use
+let state // allowed the value stored is undefined.
+
+// id=11 const can't be updated
+email="abcd@df.com"
+password="121212"
+city="Achalpur"
+
+console.log(id); 
+
+console.table([id,email,password,city,state])
