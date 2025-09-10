b746f2b7f7c3f90d6d64374eb72f24510dbaf4d7
atharvsuranje <atharvsuranje2604@gmail.com>
2025-09-11 00:13:47 +0530

    Added Comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("2">1); 
+// console.log("02">1); //converts string into number and check
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //avoid as they are a little bit undeterministic
+
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>=0);
+
+console.log("2" === 2); // === check values as well as datatype
