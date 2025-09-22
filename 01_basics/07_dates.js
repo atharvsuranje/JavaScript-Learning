20a58771dda557e41fe3ddabb2941a01780e75da
atharvsuranje <atharvsuranje2604@gmail.com>
2025-09-22 14:51:03 +0530

    Added Date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,26 @@
+let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025,8,22); //months are in 0 based indexing
+// let myCreatedDate = new Date(2025,8,22,2,36);
+// let myCreatedDate = new Date("2025-09-22"); //yyyy-mm-dd
+let myCreatedDate = new Date("09-22-2025"); //mm-dd-yyyy
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(myTimeStamp/1000)); //get seconds
+
