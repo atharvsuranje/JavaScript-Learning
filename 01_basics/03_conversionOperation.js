64522dc9d2e221633a01e4dae5a8904365fbf882
atharvsuranje <atharvsuranje2604@gmail.com>
2025-11-09 20:20:44 +0530

    Tried Some more conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,41 @@
-let score=undefined
-console.log(typeof(score))
+// let score=undefined
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(valueInNumber)
-console.log(typeof(valueInNumber))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
 
-// "10" => 10
-// "10aa" => NaN
-// "true" => 1 "false" => 0
-// null => 0
-// undefined => NaN
+// // "10" => 10
+// // "10aa" => NaN
+// // "true" => 1 "false" => 0
+// // null => 0
+// // undefined => NaN
 
 
-let isLoggedIn = "literally anything"
+// let isLoggedIn = "literally anything"
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 
 // 1 => true 0 => false
 // "" => false "literally anything" => true
 
-let num=10
-let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof(stringNum));
+let a={
+    "id":1,
+    "name":"Sachin",
+    "marks":99
+}
+for (const ele in a) {
+    console.log(`${ele}=${a[ele]}`);  
+}
+
+// console.log(1+2+"3");
+// console.log("1"+2+3);
+// console.log(1+2+"2"+3+1);
+// console.log(null==0);
+// console.log(null>0);
+// console.log(undefined>=0);
+
+
+
+
