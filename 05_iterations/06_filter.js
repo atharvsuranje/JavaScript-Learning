84f5b779e4c9dd18dc7e43475548a3915c2ed600
atharvsuranje <atharvsuranje2604@gmail.com>
2025-11-10 11:49:32 +0530

    Added Filter method

---
+++ 05_iterations/06_filter.js
@@ -0,0 +1,15 @@
+const nums=[1,2,3,4,5,6,7,8,9,10]
+
+// forEach doesn't return anything
+// const value=num.forEach( (item)=>{
+//     console.log(item);
+//     return item
+// })
+
+// let newNums=nums.filter( (num) =>{
+//     return num>4
+// })
+
+
+console.log(newNums);
+
